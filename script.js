63422c681e16f454debf650961c8112a3ae41878
Hsiao-Chi Hsieh <kida0978@gmail.com>
2025-04-03 23:46:59 +0800

    Feat: add another special-pop sound when reaching 50 clicks

--- script.js
+++ script.js
@@ -2,20 +2,23 @@ $(document).ready(function () {
     const $image = $(".image img");
     const $times = $(".times");
     const $audioNormal = $(".pop"); // 普通音效
-    const $audioSpecial = $(".special-pop"); // 特別音效
+    const $audioSpecial10 = $(".special-pop"); // 10 的倍數音效
+    const $audioSpecial50 = $(".special-pop-50"); // 50 的倍數音效
 
     // 按下滑鼠：張嘴、累計次數、播放音效
     $image.mousedown(function () {
         let countTimes = Number($times.text());
         countTimes++;
         $times.text(countTimes);
-        
         $(this).attr("src", "img/segu2.png"); // 換成張嘴的圖片
-        
-        // 檢查是否為 10 的倍數，選擇音效
-        if (countTimes % 10 === 0) {  // 嚴格相等
-            $audioSpecial[0].currentTime = 0;
-            $audioSpecial[0].play(); // 播放特殊音效
+
+        // 音效邏輯檢查
+        if (countTimes % 50 === 0) {
+            $audioSpecial50[0].currentTime = 0;
+            $audioSpecial50[0].play(); // 播放 50 的倍數音效
+        } else if (countTimes % 10 === 0) {
+            $audioSpecial10[0].currentTime = 0;
+            $audioSpecial10[0].play(); // 播放 10 的倍數音效
         } else {
             $audioNormal[0].currentTime = 0;
             $audioNormal[0].play(); // 播放普通音效
